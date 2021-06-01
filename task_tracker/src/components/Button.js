6eea04436860e5333a8cdac328dcc54563a9a0c8
7204-OMER <Eyupbhr24@gmail.com>
2021-06-01 18:08:09 +0300

    addTask

---
+++ task_tracker/src/components/Button.js
@@ -0,0 +1,17 @@
+gimport React from "react";
+
+const Button = ({ color, text, toggleShow }) => {
+  return (
+    <div>
+      <button
+        className="btn"
+        style={{ backgroundColor: color }}
+        onClick={toggleShow}
+      >
+        {text}
+      </button>
+    </div>
+  );
+};
+
+export default Button;
